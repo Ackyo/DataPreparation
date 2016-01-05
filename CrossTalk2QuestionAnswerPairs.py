8ceb71e1dd4478bf6b581999b7801647f9680d25
Ackyo <1148695202@qq.com>
2016-01-05 12:42:24 +0800

    提取有标签数据，生成训练集、无效对话对

--- CrossTalk2QuestionAnswerPairs.py
+++ CrossTalk2QuestionAnswerPairs.py
@@ -7,6 +7,8 @@ sys.setdefaultencoding('utf-8')
 import os
 import codecs
 from xml.etree import ElementTree as ET
+import re
+
 
 def FormatInvalidXmlFile(path, fileName):
     with codecs.open(path, 'r', 'utf-8') as original:
@@ -23,27 +25,56 @@ def FormatInvalidXmlFile(path, fileName):
 
 
 def FetchUtterance(preNode, curNode, fp):
-    if preNode.attrib['speaker'] != curNode.attrib['speaker']:
-        fp.write(unicode('%s %s\n' % (preNode.attrib['speaker'], preNode.text)))
-        fp.write(unicode('%s %s\n' % (curNode.attrib['speaker'], curNode.text)))
-        fp.write('\n')
+    question = preNode.text
+    # 0到2个汉字+至少0个空格+n个英文问号或感叹号+至少0个空格
+    lessThan2WordsPattern = re.compile(u"^ *[\u4e00-\u9fa5]{,2}[\?\!。、，’~]{,5}$")
+    lessThan1WordPattern = re.compile(u"^ *[\u4e00-\u9fa5]{,1}[\?\!。、，’]{,5}$")
+
+    # 去除同一个人说的两句话组成的对话、两句相同的话
+    if preNode.attrib['speaker'] != curNode.attrib['speaker'] and preNode.text != curNode.text:
+        if re.match(lessThan2WordsPattern, str(question).decode('utf8')) == None:
+            fp.write(unicode('%s %s\n' % (preNode.attrib['speaker'], preNode.text)))
+            fp.write(unicode('%s %s\n' % (curNode.attrib['speaker'], curNode.text)))
+            fp.write('\n')
+        elif re.match(lessThan1WordPattern, str(question).decode('utf8')) != None:
+            print '%s %s\n' % (preNode.attrib['speaker'], preNode.text)
+            print '%s %s\n\n' % (curNode.attrib['speaker'], curNode.text)
+            oneWordQuestionFp.write(unicode('%s %s\n' % (preNode.attrib['speaker'], preNode.text)))
+            oneWordQuestionFp.write(unicode('%s %s\n' % (curNode.attrib['speaker'], curNode.text)))
+            oneWordQuestionFp.write('\n')
+        elif re.match(re.compile(u"%s{2}" % question[0]),question):
+            print '%s %s\n' % (preNode.attrib['speaker'], preNode.text)
+            print '%s %s\n\n' % (curNode.attrib['speaker'], curNode.text)
+            twoSameWordsQuestionFp.write(unicode('%s %s\n' % (preNode.attrib['speaker'], preNode.text)))
+            twoSameWordsQuestionFp.write(unicode('%s %s\n' % (curNode.attrib['speaker'], curNode.text)))
+            twoSameWordsQuestionFp.write('\n')
+        else:
+            print '%s %s\n' % (preNode.attrib['speaker'], preNode.text)
+            print '%s %s\n\n' % (curNode.attrib['speaker'], curNode.text)
+            twoDiffWordsQuestionFp.write(unicode('%s %s\n' % (preNode.attrib['speaker'], preNode.text)))
+            twoDiffWordsQuestionFp.write(unicode('%s %s\n' % (curNode.attrib['speaker'], curNode.text)))
+            twoDiffWordsQuestionFp.write('\n')
+
 
 def FetchAction(preNode, curNode, fp):
     fp.write(unicode('%s\n' % preNode.text))
-    fp.write(unicode('%s\n' % curNode.text))
+    fp.write(unicode('%s\n\n' % curNode.text))
+
 
 data = "D:\PythonProject\剧本数据"
 data = unicode(data, "utf-8")
 
-invalidXmlFileFp = codecs.open(u'D:\PythonProject\InvalidXmlFile.txt', 'w', 'utf-8')
+invalidXmlFileFp = codecs.open(u'D:\\PythonProject\\InvalidXmlFile.txt', 'w', 'utf-8')
 # noConversation = codecs.open(u'D:\\PythonProject\\noConversation.txt', 'w', 'utf-8')
 notHandledFp = codecs.open(u'D:\\PythonProject\\notHandled.txt', 'w', 'utf-8')
 
+oneWordQuestionFp = codecs.open(u'D:\\PythonProject\\oneWordQuestion.txt', 'w', 'utf-8')
+twoDiffWordsQuestionFp = codecs.open(u'D:\\PythonProject\\twoDiffWordsQuestion.txt', 'w', 'utf-8')
+twoSameWordsQuestionFp = codecs.open(u'D:\\PythonProject\\twoSameWordsQuestion.txt', 'w', 'utf-8')
 resultPath = u'D:\\PythonProject\\QAPairs'
 
 if os.path.exists(resultPath) == False:
     os.mkdir(resultPath)
-
 
 for basePath, dirs, files in os.walk(data):
     for file in files:
@@ -60,7 +91,7 @@ for basePath, dirs, files in os.walk(data):
                 tree = ET.parse(filePath)
             except Exception, e:
                 print '%s not handled -- invalid reformat xml file ' % filePath
-                notHandledFp.write(filePath+'\n')
+                notHandledFp.write(filePath + '\n')
                 isValidXmlFile == False
                 invalidXmlFileFp.write(filePath + '\n')
 
@@ -78,7 +109,7 @@ for basePath, dirs, files in os.walk(data):
 
             preNode = 0
 
-            qaPair=os.path.join(resultPath, '%s-singleQAPair.txt' % str(file).split('.')[0])
+            qaPair = os.path.join(resultPath, '%s-singleQAPair.txt' % str(file).split('.')[0])
             qaPairFp = codecs.open(qaPair, 'w', 'utf-8')
 
             utteranceCounts = 0
@@ -89,18 +120,14 @@ for basePath, dirs, files in os.walk(data):
                 if curNode.tag == 'utterance':
                     # print curNode.attrib['speaker'], curNode.text
                     FetchUtterance(preNode, curNode, qaPairFp)
-                    preNode=curNode
+                    preNode = curNode
                     utteranceCounts += 1
-                    handled=True
-                    # elif curNode.tag == 'action':
-                    #     print curNode.text
-                    #     FetchAction(preNode, curNode, queryAnswerPairs)
-                    # else:
-                        # print '******************************** not Find: (%s) **************************************' % filePath
-            #处理没有utterance标签，以及仅一个utterance标签和多个action标签的情况
-            if handled == False or utteranceCounts==1:
-                if utteranceCounts==1:
-                    session_node=root
+                    handled = True
+
+            # 处理没有utterance标签，以及仅一个utterance标签和多个action标签的情况
+            if handled == False or utteranceCounts == 1:
+                if utteranceCounts == 1:
+                    session_node = root
                 for curNode in session_node:
                     if curNode.tag == 'action':
                         if preNode == 0:
@@ -108,14 +135,17 @@ for basePath, dirs, files in os.walk(data):
                             continue
                         # print curNode.text
                         FetchAction(preNode, curNode, qaPairFp)
-                        preNode=curNode
+                        preNode = curNode
                         handled = True
-            #其他特殊情况
+            # 其他特殊情况
             qaPairFp.close()
             if handled == False:
                 print '%s not handled' % filePath
                 os.remove(qaPair)
-                notHandledFp.write(filePath+'\n')
+                notHandledFp.write(filePath + '\n')
 
 invalidXmlFileFp.close()
 notHandledFp.close()
+oneWordQuestionFp.close()
+twoDiffWordsQuestionFp.close()
+twoSameWordsQuestionFp.close()
---
+++ SplitQAParis2TrainingSet.py
@@ -0,0 +1,93 @@
+# coding=utf-8
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import re
+import os
+import codecs
+
+
+def StripPrelude(srcStr):
+    try:
+        prelude = u'%s' % re.findall(r'^.*?(?= )', srcStr.decode('utf8'))[0].strip()
+        return srcStr.decode('utf8').lstrip(prelude).strip()
+    except Exception, e:
+        print 'Abnormal dialogue:>>>>>>>>>>>%s>>>>>>>>>>>' % srcStr.strip()
+    return srcStr.strip()
+
+
+srcPath = "D:\PythonProject\LabeledData"
+srcPath = unicode(srcPath, "utf-8")
+
+resultPath = "D:\PythonProject\FinishPreProcess"
+
+if os.path.exists(resultPath) == False:
+    os.mkdir(resultPath)
+
+
+def SplitQAParis2TrainingSet(srcPath, resultPath):
+    # 分成两个文件，分别存放合法对话与不合法对话
+    validDialogQuestionFileName = 'validDialog-Question.txt'
+    validDialogAnswerFileName = 'validDialog-Answer.txt'
+
+    validDialogQuestionPath = os.path.join(resultPath, validDialogQuestionFileName)
+    validDialogAnswerPath = os.path.join(resultPath, validDialogAnswerFileName)
+
+    validDialogQuestionFp = codecs.open(validDialogQuestionPath, 'w', 'utf8')
+    validDialogAnswerFp = codecs.open(validDialogAnswerPath, 'w', 'utf8')
+
+    validQAPairsFp = codecs.open(os.path.join(resultPath,'validQAParis.txt'),'w','utf8')
+    invalidQAPairsFp = codecs.open(os.path.join(resultPath,'invalidQAPairs.txt'),'w','utf8')
+
+    for basePath, dirs, files in os.walk(srcPath):
+        singleDialogTotalCount = 0
+        singleDialogValidCount = 0
+        for file in files:
+            filePath = os.path.join(basePath, file)
+            # 每次读三行，若前两行都有内容：第三行为1，第三行为0
+            fp = codecs.open(filePath, 'r', 'utf8')
+            line1 = fp.readline()
+            line2 = fp.readline()
+            line3 = fp.readline()
+            print file,':'
+            # print line1,line2,line3
+
+            total=0
+            validCount=0
+
+            while line3:
+                total += 1
+                question = StripPrelude(line1)
+                answer = StripPrelude(line2)
+
+                if line3 == '1\n':
+                    invalidQAPairsFp.write(question+'\n')
+                    invalidQAPairsFp.write(answer+'\n\n')
+                else:
+                    validCount += 1
+                    validDialogQuestionFp.write(question+'\n')
+                    validDialogAnswerFp.write(answer+'\n')
+
+                    validQAPairsFp.write(question+'\n')
+                    validQAPairsFp.write(answer+'\n\n')
+
+                line1 = fp.readline()
+                line2 = fp.readline()
+                line3 = fp.readline()
+                # print line1,line2,line3
+            singleDialogTotalCount += total
+            singleDialogValidCount += validCount
+
+            print '<<<<<<< Total:%d  <<<<<< Valid:%d  <<<<<< Invalid: %d <<<<<<' % (total,validCount,total-validCount)
+            fp.close()
+    print '>>>>>>>>>>>>>>>>>>> Total dialogues: %d >>>>>> Valid: %d >>>>>> Invalid: %d >>>>>>> ValidRate: %.2f%% >>>>>>>>' % (singleDialogTotalCount,singleDialogValidCount,singleDialogTotalCount-singleDialogValidCount,singleDialogValidCount*100.0/singleDialogTotalCount)
+    validDialogAnswerFp.close()
+    validDialogQuestionFp.close()
+    validQAPairsFp.close()
+    invalidQAPairsFp.close()
+
+
+if __name__ == '__main__':
+    SplitQAParis2TrainingSet(srcPath, resultPath)
